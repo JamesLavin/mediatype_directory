d5c87f3a2fad5e9966362589fd0afd48af34594b
James Lavin <james@jameslavin.com>
2012-08-28 10:24:44 -0400

    many improvements to mediatype_directory.rb

--- lib/mediatype_directory/mediatype_directory.rb
+++ lib/mediatype_directory/mediatype_directory.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'fileutils'
+require 'pathname'
 
 # MediatypeDirectory.new takes a configuration hash and creates an object with a create_directory() method
 # Calling create_directory() on the MediatypeDirectory object creates a directory, configured according to 
@@ -8,6 +9,146 @@ require 'fileutils'
 #
 # Usage:
 # MediatypeDirectory.new(options_hash).create_directory
+#
+# Alternative usage:
+# md = MediatypeDirectory.new
+# md.property = value (for all desired property-value pairs)
+# md.create_directory
+#
+# Example:
+# To create a directory at '~/Tech/Docs/PDFs' with softlinks to all PDF and ODT files under '~/Tech':
+# md = MediatypeDirectory.new
+# md.extensions        = ['.pdf','.odt']
+# md.mediatype_dirname = '~/Tech/Docs/PDFs'
+# md.directory_tree    = '~/Tech'
+# md.create_directory
+#
 class MediatypeDirectory
+
+  attr_accessor :extensions, :linktype
+  attr_reader :mediatype_dirname, :directory_tree
+
+  class InvalidDirname < StandardError
+  end
+
+  def initialize(config)
+    @extensions             = config[:extensions]
+    self.mediatype_dirname  = config[:mediatype_dirname]
+    self.directory_tree     = config[:directory_tree]
+    @linktype               = config[:linktype] || 'soft'
+  end
+
+  def create_directory
+    check_directories
+    Dir.chdir(mediatype_dirname)
+    create_softlinks
+  end
+
+  def mediatype_dirname=(dirname)
+    @mediatype_dirname = nil_or_convert_dirname(dirname)
+  end
+
+  def directory_tree=(dirname)
+    @directory_tree = nil_or_convert_dirname(dirname)
+  end
+
+  private
+
+  def hardlinks?
+    @linktype == 'hard'
+  end
+
+  def create_softlinks
+    get_all_mediatype_files
+    #puts "Found these files: " + @mediatype_files.to_s
+    hardlinks? ? mediatype_files_to_hardlinks : mediatype_files_to_softlinks
+  end
+
+  # stores array of files to be softlinked in @mediatype_files
+  def get_all_mediatype_files
+    puts "Searching for files in #{directory_tree}"
+    Dir.chdir(directory_tree)
+    mediatype_files = []
+    @extensions.each do |ex|
+      search_for = File.join("**", '*' + ex)        # example: "**/*.pdf"
+      mediatype_files.concat(Dir.glob(search_for))
+    end
+    puts "Found these files: " + mediatype_files.to_s
+    @mediatype_files = mediatype_files.map { |mf| Pathname.new(mf).realpath }
+  end
+
+  def mediatype_files_to_softlinks
+    Dir.chdir(mediatype_dirname)
+    @mediatype_files.each do |pathname|
+      mediatype_file_to_softlink pathname
+    end
+  end
+
+  def mediatype_files_to_hardlinks
+    Dir.chdir(mediatype_dirname)
+    @mediatype_files.each do |pathname|
+      mediatype_file_to_hardlink pathname
+    end
+  end
+
+  def mediatype_file_to_softlink(pathname)
+    softlink = Pathname.new(@mediatype_dirname) + pathname.basename
+    if File.exists?(softlink.to_s)
+      puts "WARNING: #{softlink.to_s} already exists"
+    else
+      puts "Creating #{softlink.to_s}"
+      #`ln -s #{pathname.to_s} #{softlink.to_s}`
+      FileUtils.ln_s(pathname.to_s, softlink.to_s)
+    end
+  end
+
+  def mediatype_file_to_hardlink(pathname)
+    hardlink = Pathname.new(@mediatype_dirname) + pathname.basename
+    if File.exists?(hardlink.to_s)
+      puts "WARNING: #{hardlink.to_s} already exists"
+    else
+      puts "Creating #{hardlink.to_s}"
+      #`ln #{pathname.to_s} #{hardlink.to_s}`
+      FileUtils.ln(pathname.to_s, hardlink.to_s)
+    end
+  end
+
+  def nil_or_convert_dirname(dirname)
+    (dirname.nil? || dirname == '') ? nil : convert_dirname(dirname)
+  end
+
+  def check_directories
+    validate_directories
+    create_missing_directories
+  end
+
+  def create_missing_directories
+    [mediatype_dirname, directory_tree].each do|dn|
+      make_dirname(dn)
+    end
+  end
+
+  def validate_directories
+    [mediatype_dirname, directory_tree].each do|dn|
+      validate_dirname(dn)
+    end
+  end
+
+  def make_dirname(dn)
+    unless File.directory? dn
+      puts "Creating directory #{dn}"
+      FileUtils.mkdir_p dn
+    end
+  end
+
+  # Ideally, should use a regexp that matches valid directories
+  # For now, a simple sanity check
+  def validate_dirname(dirname)
+    raise MediatypeDirectory::InvalidDirname, "#{dirname} is not a valid directory name" if dirname.match(/\s/) || !dirname.match(/^\//)
+  end
+
+  def convert_dirname(dirname)
+    File.expand_path(dirname)
+  end
 
 end
